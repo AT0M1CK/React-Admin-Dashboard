1505c19d3e8abfc2a3a74f28dec3bb767d59ca5a
Akshayanand Vm <50578479+AT0M1CK@users.noreply.github.com>
2023-01-04 19:12:22 +0530

    refactor: Added Large & Small state to the sideBar

--- admin-dashboard/src/components/SideBar.js
+++ admin-dashboard/src/components/SideBar.js
@@ -7,9 +7,10 @@ import {
   Text,
 } from "@chakra-ui/react";
 import { FiMenu } from "react-icons/fi";
-import React from "react";
+import React, { useState } from "react";
 
 const SideBar = () => {
+  const [navSize, setNavSize] = useState("large");
   return (
     <Flex
       pos="sticky"
@@ -17,26 +18,45 @@ const SideBar = () => {
       h="95vh"
       marginTop="2.5vh"
       boxShadow="0 4px 12px 0 rgba(0, 0, 0, 0.05)"
-      w="200px"
+      borderRadius={navSize === "small" ? "15px" : "30px"}
+      w={navSize === "small" ? "75px" : "200px"}
       flexDir="column"
       justifyContent="space-between"
     >
-      <Flex p="5%" flexDir="column" alignItems="flex-start" as="nav">
+      <Flex
+        p="5%"
+        flexDir="column"
+        alignItems={navSize === "small" ? "center" : "flex-start"}
+        as="nav"
+      >
         <IconButton
           background="none"
           mt={5}
           _hover={{ background: "none" }}
           icon={<FiMenu />}
-          onClick={() => {}}
+          onClick={() => {
+            if (navSize === "small") setNavSize("large");
+            else setNavSize("small");
+          }}
         />
       </Flex>
 
-      <Flex p="5%" flexDir="column" w="100%" alignItems="flex-start" mb={4}>
-        <Divider />
+      <Flex
+        p="5%"
+        flexDir="column"
+        w="100%"
+        alignItems={navSize === "small" ? "center" : "flex-start"}
+        mb={4}
+      >
+        <Divider display={navSize === "small" ? "none" : "flex"} />
 
         <Flex mt={4} align="center">
           <Avatar size="sm" src="avatar-1.png" />
-          <Flex flexDir="column" ml={4}>
+          <Flex
+            flexDir="column"
+            ml={4}
+            display={navSize === "small" ? "none" : "flex"}
+          >
             <Heading as="h3" size="sm">
               Test Admin
             </Heading>
